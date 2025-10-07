101182713c7ca7f12b74ccfdeccd61acbf62c537
1theory <hybrid7theory@gmail.com>
2025-10-07 12:24:16 +0200

    Added Javelin address

---
+++ zoeAddresses.py
@@ -0,0 +1,24 @@
+CHECK_TYPE = {
+    "bit": 0,
+    "int": 1,
+    "uint": 2,
+    "byte": 3,
+    "short": 4,
+    "falseBit": 5,
+    "long": 6,
+    "nibble": 7,
+}
+COMPARE_TYPE = {
+    "Match": 0,
+    "GreaterThan": 1,
+    "LessThan": 2,
+}
+
+ADDRESSES = {
+    "SLUS-20148": {
+        "Weapons": {
+            "Javelin": {"unlockAddress": 0x0048EF24, "id": 0, "ammoAddress": 0x0048EF2E, "lv1Ammo": 50, },
+        }
+
+    }
+}
